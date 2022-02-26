77d895a15b6d52a20d8c0fad4c46dfee5ca7a132
PyxelCraft <99120594+PyxelCraft@users.noreply.github.com>
2022-02-26 13:56:36 +0700

    Update to Latest Dart 2.16.1
    Update to Latest Dart 2.16.1 Syntax and some linter warning fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/all.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'movie_app.dart';
 
 void main() {
-  runApp(ProviderScope(child: MovieApp()));
+  runApp(const ProviderScope(child: MovieApp()));
 }
